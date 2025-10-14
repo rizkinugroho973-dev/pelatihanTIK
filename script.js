f9e6c533b15708e53df9bfd18ee5fa9aeb867a9b
rizkinugroho973-dev <rizkinugroho973@gmail.com>
2025-10-14 11:41:44 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const tombolSapa = document.getElementbyld('sapaButton'):
+tombolSapa.addEventListener('click',fuction(){
+                            alert('Halo! terimakasih sudah berkunjung!');
+})
